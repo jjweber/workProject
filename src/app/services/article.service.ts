2c1f9dbf1351cfe11e6021a5dd6b12b1514c20a6
Your Name <you@example.com>
2017-09-22 14:37:31 -0400

    Added comments on article.service.ts file. Moved a few variables down below export.

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -4,35 +4,40 @@ import { Observable } from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
 
-const apiKey = '0d7756c1218343da88921149462e1cb7';
-const baseUrl = 'https://newsapi.org/v1/articles';
-
-const searchField = {
-  value: 'bild'
-};
-
 @Injectable()
 export class ArticleService {
+
+  // Declaring variables.
+  apiKey = '0d7756c1218343da88921149462e1cb7';
+  baseUrl = 'https://newsapi.org/v1/articles';
   articlesUrl: any = '';
   currentSource: any = 'abc-news-au';
 
   constructor(private jsonp: Jsonp, private http: Http) {}
 
+  // Function makes initial call to news api to populate page with articles when page loads.
   getArticles() {
-    this.articlesUrl = baseUrl + '?sortBy=top&source=' +
-     this.currentSource + '&apiKey=' + apiKey;
+    // Building some variables to make api call.
+    this.articlesUrl = this.baseUrl + '?sortBy=top&source=' +
+     this.currentSource + '&apiKey=' + this.apiKey;
     console.log('Calling api: ', this.articlesUrl);
 
+    // Making call to api for articles from ABC news.
     return this.http.get(this.articlesUrl)
+      // Mapping the results to json.
       .map((res: any) => res.json());
   }
 
+  // Function that is passed a newsSource to make an additional call to news api to populate page when news source has changed.
   getArticlesBYNewsSource(newsSource: string) {
-    this.articlesUrl = baseUrl + '?sortBy=top&source=' +
-     newsSource + '&apiKey=' + apiKey;
+    // Building some variables to make api call.
+    this.articlesUrl = this.baseUrl + '?sortBy=top&source=' +
+     newsSource + '&apiKey=' + this.apiKey;
     console.log('Calling api: ', this.articlesUrl);
 
+    // Making call to api for articles from selected source.
     return this.http.get(this.articlesUrl)
+    // Mapping the results to json.
       .map((res: any) => res.json());
   }
 }
